10ed9c35fc99ab90e46f0209bd891412f424b42f
Paras14 <53565432+Paras14@users.noreply.github.com>
2023-09-28 21:44:02 +0200

    change db configs

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
-    $pdo = new PDO('mysql:host=localhost;dbname=message_board', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=message_board_database', 'message_user', 'testpass');
 ?>
